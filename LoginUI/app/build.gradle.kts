bc9f9cdf7257e988334a10edeea38819dd476e76
Omar Flores <floresquispeomar8@gmail.com>
2025-04-04 23:58:36 -0500

    Primer commit

---
+++ LoginUI/app/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.juanitodev.loginui"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.juanitodev.loginui"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.konfetti)
+    implementation(libs.androidx.navigation.compose)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("io.ktor:ktor-client-core:2.3.7")
+    implementation("com.google.code.gson:gson:2.10.1")
+    // Choose the appropriate engine (e.g., for Android)
+    implementation("io.ktor:ktor-client-android:2.3.7")
+    implementation("io.ktor:ktor-client-cio:2.3.7")
+    // Optional: JSON serialization
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.7")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
+    implementation("com.auth0:java-jwt:4.4.0")
+
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# Interfaz de Login en Kotlin
+
+Agregar una conexion a la api de gandy
+
+### Amarillo
+* Juan Piero
+* Dulio Quispe
+
+## 📱 Capturas de pantalla
+
+![estudiante](https://raw.githubusercontent.com/juanitoeldesastre/LoginUI/main/captures/login_estudiante.PNG)
+![welcome](https://raw.githubusercontent.com/juanitoeldesastre/LoginUI/main/captures/welcome.PNG)
+![vigilante](https://raw.githubusercontent.com/juanitoeldesastre/LoginUI/main/captures/login_vigilante.PNG)
+
+## 🛠️Tecnologías
+
+- **Kotlin**
+- **Jetpack Compose**
+- **Android Studio**
+- **Material 3**
+
+## 🚧 En Construccion
